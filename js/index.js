5c5e4ce5843304b8adbdb10c62a0f8dc44806570
Oleg Teslenko <orlantostello@gmail.com>
2021-07-04 14:28:18 +0300

    коррекция

--- js/index.js
+++ js/index.js
@@ -5,11 +5,8 @@ const modalEl = document.querySelector('.js-lightbox');
 const modalOverlay = document.querySelector('.lightbox__overlay');
 const modalImg = document.querySelector('.lightbox__image');
 const closeModalBtn = document.querySelector('[data-action="close-lightbox"]');
-const body = document.querySelector('body');
 
 const cardsMarkup = makeGalleryMarkup(galleryItems);
-
-let currentIndex = 0;
 
 galleryList.insertAdjacentHTML('beforeend', cardsMarkup);
 
@@ -18,9 +15,8 @@ function makeGalleryMarkup(images) {
     .map(({ preview, original, description }) => {
       return `<li class="gallery__item">
   <a class="gallery__link" href="${original}">
-    <img
-      loading="lazy"
-      class="gallery__image lazyload"
+    <img      
+      class="gallery__image"
       src="${preview}"
       data-source="${original}"
       alt="${description}"
@@ -35,7 +31,6 @@ function makeGalleryMarkup(images) {
 galleryList.addEventListener('click', onCardClick);
 modalOverlay.addEventListener('click', onOverlay);
 closeModalBtn.addEventListener('click', closeModal);
-// modalEl.addEventListener('click', closeModal);
 
 function onCardClick(e) {
   e.preventDefault();
@@ -44,30 +39,20 @@ function onCardClick(e) {
     return;
   }
 
-  //   console.log(e.target.dataset.source);
-  //     openModal(e.target.dataset.source);
   openModal(e);
 }
 
 function openModal(e) {
   modalEl.classList.add('is-open');
-  body.classList.add('is-open');
 
   modalImg.src = e.target.dataset.source;
   modalImg.alt = e.target.alt;
-
-  galleryItems.forEach(el => {
-    if (el.original === modalImg.src) {
-      currentIndex = galleryItems.indexOf(el);
-    }
-  });
 
   window.addEventListener('keydown', onListenerButton);
 }
 
 function closeModal() {
   modalEl.classList.remove('is-open');
-  body.classList.remove('is-open');
   modalImg.src = '';
 }
 
@@ -79,12 +64,8 @@ function onOverlay(e) {
   closeModal();
 }
 
-// window.addEventListener('keydown', onListenerButton);
-
 function onListenerButton(e) {
   const ESCAPE = 'Escape';
-  const ARROWRIGHT = 'ArrowRight';
-  const ARROWLEFT = 'ArrowLeft';
   const isArrowRight = e.code === 'ArrowRight';
   const isArrowLeft = e.code === 'ArrowLeft';
 
@@ -93,23 +74,15 @@ function onListenerButton(e) {
     closeModal();
   }
 
-  //   if (isArrowRight) {
-  //     showNextImg(isArrowRight);
-  //   }
-
-  //   if (isArrowLeft) {
-  //     showNextImg(isArrowLeft);
-  //   }
-
-  if (ARROWRIGHT || ARROWLEFT) {
+  if (isArrowRight || isArrowLeft) {
     showNextImg(isArrowRight);
   }
 }
 
-const imglArr = galleryItems.map(img => img.original);
+const imgArr = galleryItems.map(img => img.original);
 function showNextImg(direction) {
   let index;
-  let currentImg = imglArr.indexOf(modalImg.src);
+  let currentImg = imgArr.indexOf(modalImg.src);
   index = direction ? currentImg + 1 : currentImg - 1;
   if (index < 0) {
     index = galleryItems.length + index;
@@ -117,5 +90,5 @@ function showNextImg(direction) {
   if (index === galleryItems.length) {
     index = 0;
   }
-  modalImg.src = imglArr[index];
+  modalImg.src = imgArr[index];
 }
